4c76610e286c05e456b2dfbec59c403eede33d38
pc <nikitamilovatskiy@gmail.com>
2023-06-13 00:32:28 +0300

    style

--- internal/buttonLogic/buttonLogic.go
+++ internal/buttonLogic/buttonLogic.go
@@ -13,15 +13,15 @@ func SignIn(inputLogin, inputPassword *widget.Entry, window fyne.Window, content
 	login := inputLogin.Text
 	password := inputPassword.Text
 
-	wrongData := canvas.NewText("Incorrect login/password", color.RGBA{R: 255, A: 255})
-	wrongData.TextStyle = fyne.TextStyle{Italic: true}
+	wrongDataMessage := canvas.NewText("Incorrect login/password", color.RGBA{R: 255, A: 255})
+	wrongDataMessage.TextStyle = fyne.TextStyle{Italic: true}
 
 	if len(content.Objects) > 3 {
 		content.Objects = content.Objects[:len(content.Objects)-1]
 	}
 
 	if !(sendLoginSign(login) && sendPasswordSign(login, password)) {
-		content.Add(wrongData)
+		content.Add(wrongDataMessage)
 		window.SetContent(content) // Обновление содержимого окна
 		window.Resize(window.Canvas().Size())
 		return false
