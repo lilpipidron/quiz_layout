d021f7ef4da69e83702fa757e876709d1cd6d5a1
pc <nikitamilovatskiy@gmail.com>
2023-06-14 22:07:12 +0300

    updated logging, but error on truncate - TODO fix

--- internal/ui/game/window.go
+++ internal/ui/game/window.go
@@ -9,19 +9,76 @@ import (
 	"game_with_Nikita/internal/file_work"
 	"image/color"
 	"log"
+	"os"
 	"strconv"
 )
 
-const (
-	First int = iota + 1
-)
+var currentStage int
 
-var currentState int
-
-func LayoutSetter(stage int, content *fyne.Container, taskText, login string) {
+func LayoutSetter(content *fyne.Container, taskText, login string) {
 	content.RemoveAll()
-	currentState = stage
-	stageText := canvas.NewText(strconv.Itoa(stage)+" Stage", color.RGBA{R: 150, G: 150, B: 150, A: 150})
+	filename := login + "-log.txt"
+
+	if currentStage == 1 {
+		if _, err := os.Stat(filename); err == nil {
+			file, err2 := os.Open(filename)
+			if err2 != nil {
+				log.Fatal(err2)
+			}
+			defer func(logFile *os.File) {
+				err2 = logFile.Close()
+				if err2 != nil {
+					log.Fatal(err2)
+				}
+			}(file)
+
+			scanner := bufio.NewScanner(file)
+			var lines []string
+			for scanner.Scan() {
+				lines = append(lines, scanner.Text())
+			}
+			if len(lines) >= 3 {
+				if tmp, _ := strconv.Atoi(lines[len(lines)-1]); tmp > currentStage {
+					currentStage = tmp
+				}
+				lines = lines[:len(lines)-1]
+			}
+			lines = append(lines, strconv.Itoa(currentStage))
+			if err3 := file.Truncate(0); err3 != nil {
+				log.Fatal(err3)
+			}
+			if _, err3 := file.Seek(0, 0); err3 != nil {
+				log.Fatal(err3)
+			}
+			writer := bufio.NewWriter(file)
+			for _, line := range lines {
+				if _, err4 := writer.WriteString(line + "\n"); err4 != nil {
+					log.Fatal(err4)
+				}
+			}
+			if err4 := writer.Flush(); err4 != nil {
+				log.Fatal(err4)
+			}
+		} else {
+			log.Fatal(err)
+		}
+	} else {
+		logFile := file_work.OpenWrite(filename)
+		defer file_work.CloseFunc(logFile)
+		writer := bufio.NewWriter(logFile)
+		defer func(writer *bufio.Writer) {
+			err := writer.Flush()
+			if err != nil {
+				log.Fatal(err)
+			}
+		}(writer)
+		_, err := fmt.Fprintln(writer, currentStage)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	stageText := canvas.NewText(strconv.Itoa(currentStage)+" Stage", color.RGBA{R: 150, G: 150, B: 150, A: 150})
 	stageText.TextStyle = fyne.TextStyle{Bold: true}
 	stageText.TextSize = 48
 	content.Add(stageText)
@@ -29,25 +86,10 @@ func LayoutSetter(stage int, content *fyne.Container, taskText, login string) {
 	newText := widget.NewLabel(taskText)
 	newText.Wrapping = fyne.TextWrapWord
 	content.Add(newText)
-
-	filename := login + "-log.txt"
-	logFile := file_work.OpenWrite(filename)
-	defer file_work.CloseFunc(logFile)
-	writer := bufio.NewWriter(logFile)
-	defer func(writer *bufio.Writer) {
-		err := writer.Flush()
-		if err != nil {
-			log.Fatal(err)
-		}
-	}(writer)
-	_, err := fmt.Fprintln(writer, currentState)
-	if err != nil {
-		log.Fatal(err)
-	}
 }
 
 func FirstStageLayout(content *fyne.Container, login string) {
-	stage := First
+	currentStage = 1
 
-	LayoutSetter(stage, content, "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", login)
+	LayoutSetter(content, "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", login)
 }
