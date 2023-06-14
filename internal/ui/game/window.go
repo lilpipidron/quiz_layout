9378a3d7cb605c9c3a531651b3b40f401325e412
pc <nikitamilovatskiy@gmail.com>
2023-06-14 21:15:12 +0300

    rename

--- internal/ui/game/window.go
+++ internal/ui/game/window.go
@@ -17,7 +17,7 @@ const (
 
 var currentState int
 
-func layoutSetter(stage int, content *fyne.Container, taskText, login string) {
+func LayoutSetter(stage int, content *fyne.Container, taskText, login string) {
 	content.RemoveAll()
 	currentState = stage
 	stageText := canvas.NewText(strconv.Itoa(stage)+" Stage", color.RGBA{R: 150, G: 150, B: 150, A: 150})
@@ -37,11 +37,7 @@ func layoutSetter(stage int, content *fyne.Container, taskText, login string) {
 			log.Fatal(err)
 		}
 	}(writer)
-	_, err := fmt.Fprintln(writer, login)
-	if err != nil {
-		log.Fatal(err)
-	}
-	_, err = fmt.Fprintln(writer, currentState)
+	_, err := fmt.Fprintln(writer, currentState)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -50,5 +46,5 @@ func layoutSetter(stage int, content *fyne.Container, taskText, login string) {
 func FirstStageLayout(content *fyne.Container, login string) {
 	stage := First
 
-	layoutSetter(stage, content, "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", login)
+	LayoutSetter(stage, content, "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", login)
 }
